03a7c5c72d283115103867f8e696c032c960b761
jorgefdt <jorge@viafoura.com>
2016-03-27 20:43:41 -0400

    test 2

--- api/iOS/VCSimpleSession.mm
+++ api/iOS/VCSimpleSession.mm
@@ -472,7 +472,7 @@ namespace videocore { namespace simpleApi {
                        cameraState:(VCCameraState) cameraState
                         aspectMode:(VCAspectMode)aspectMode
 {
-    printf(">>>>>>> JORGE SUBLIME\n\n");
+    printf(">>>>>>> JORGE SUBLIME 2\n\n");
 
     self.bitrate = bps;
     self.videoSize = videoSize;
